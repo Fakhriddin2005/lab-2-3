bcc0a573d86377ef1585b3d1bd361d18626a6ca7
Fakhriddin2005 <fakhriddinabdurahimov8@gmail.com>
2024-02-29 11:17:45 +0300

    new

--- lab.py
+++ lab.py
@@ -1,1 +1,8 @@
-print: ("HALO")
+words=[]
+N=(input ("Введите слово"))
+while N!="stop":
+    words.append(N)
+    N=input("Введите следующее слово")
+result="".join(words)
+print("Результат")
+print(result)
